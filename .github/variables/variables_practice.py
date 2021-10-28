026a2e7fec42488129ab8b3b4293653a6475ac29
Alexis l <90405920+s20049733@users.noreply.github.com>
2021-10-28 12:05:50 -0400

    verson 0.1
    strings and floats

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,1 +1,11 @@
 #variable pratice / alexis labrada version0.0 
+
+#varible types -- most common
+
+my_float= -1.23455
+mr_int= -19283761234
+my_string=  "i dont like turtles"
+
+print(my_float)
+print(my_int)
+print(my_string)
